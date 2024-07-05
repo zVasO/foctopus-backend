955586f07671b45226da9234d76d676d6512dddc
zVasO <dev.dyger@gmail.com>
2024-07-05 19:54:37 +0200

    api: add name + a new route for show noises

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,15 @@
 
 use App\Http\Controllers\API\LoginController;
 use App\Http\Controllers\API\LogoutController;
+use App\Http\Controllers\API\NoiseController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LogoutController::class, 'logout']);
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
+
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/users', [UserController::class, 'getAll']);
+    Route::get('/users', [UserController::class, 'getAll'])->name('getAllUsers');
+    Route::get('/noises', [NoiseController::class, 'getAll'])->name('getAllNoises');
 });
